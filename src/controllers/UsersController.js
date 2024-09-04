31a0b8405a46d6732a52965b67629eaaa8e49ea9
RastaDev21 <humberto.simoes@whaticket.com>
2024-09-04 17:59:34 -0300

    Criação do banco dados e criação da tabela de usuarios

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   create(request, response) {
     const { name, rg, cpf, dateOfBirth } = request.body;
+
+    if (!name) {
+      throw new AppError("Nome é obrigatório");
+    }
 
     response.status(201).json({ name, rg, cpf, dateOfBirth });
   }
---
+++ src/database/sqlite/migrations/createUsers.js
@@ -0,0 +1,14 @@
+const createUsers = `
+    CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+  	name VARCHAR,
+  	Rg VARCHAR,
+		Cpf VARCHAR,
+		data_de_nascimento VARCHAR,
+  	password VARCHAR,
+  	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+`;
+
+module.exports = createUsers;
